7330714a444768d4ed7ef389de791efa9dd8c12f
Omar Rehan <omarrehan7777777@gmail.com>
2026-02-10 20:28:52 +0200

    fix: remove invalid React import in TaskContext

--- src/contexts/TaskContext.jsx
+++ src/contexts/TaskContext.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import { createContext, useState, user } from "react";
+import { createContext, useState } from "react";
 
 
 export const TaskContext = createContext("default")
